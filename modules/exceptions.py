5054905e47c6bd62fa9fcf8cfcffd2116a7ccbfa
Simone Marzona <simone.marzona@knoway.info>
2016-01-17 17:58:35 +0000

    removed the occurrences of gqrx-remote and replaced with the new project name "rig-remote"

--- modules/exceptions.py
+++ modules/exceptions.py
@@ -1,5 +1,20 @@
 #!/usr/bin/env python
+"""
+Remote application that interacts with rigs using rigctl protocol.
 
+Please refer to:
+http://gqrx.dk/
+http://gqrx.dk/doc/remote-control
+http://sourceforge.net/apps/mediawiki/hamlib/index.php?title=Documentation
+
+Author: Rafael Marmelo
+Author: Simone Marzona
+
+License: MIT License
+
+Copyright (c) 2014 Rafael Marmelo
+Copyright (c) 2015 Simone Marzona
+"""
 # custom exception definition
 
 # non retriable custom exceptions
