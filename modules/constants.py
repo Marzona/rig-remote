650bd3e8ffe104e5165ff0f48ad4cb490514adf8
Simone Marzona <marzona@knoway.info>
2016-02-06 15:53:37 +0000

    updated the config file adding the config directive for the number of loops for monitoring mode, added the code for bookmark scan monitoring. Completed the mods to the UI creating a new area that holds all the options that are shared between both frequency scan and bookmarks scan. NOTE: The UI needs to be rewritten from scratch.

--- modules/constants.py
+++ modules/constants.py
@@ -46,7 +46,7 @@ UNKNOWN_MODE = "unknown"
 # monitoring mode delay
 MONITOR_MODE_DELAY = 2
 # monitoring mode loops count
-MONITOR_MODE_LOOPS = 10
+
 # dictionary for mapping between rig modes and rig-remote modes
 # the key is the rig-remote namings and the value is the rig naming
 
@@ -75,4 +75,5 @@ DEFAULT_CONFIG = {"hostname" : "127.0.0.1",
                   "range_max" : "1800,000",
                   "always_on_top" : "True",
                   "save_exit" : "False",
-                  "auto_bookmark" : "False"}
+                  "auto_bookmark" : "False",
+                  "monitor_mode_loops" : 10}
