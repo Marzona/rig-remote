bce473ecda1b6d3b0441a1788b95c61fc01ce92b
Simone Marzona <marzona@knoway.info>
2016-04-25 16:54:58 +0100

    small updates on docstrings, some changes in queue handling and configuration

--- modules/stmessenger.py
+++ modules/stmessenger.py
@@ -19,12 +19,13 @@ TAS - Tim Sweeney - mainetim@gmail.com
 
 2016/04/29 - TAS - STMessenger class created to facilitate specific comm needs between scan and
                    main threads, using QueueComms class.
-
 """
 
-
-
 from queue_comms import QueueComms
+import logging
+
+# logging configuration
+logger = logging.getLogger(__name__)
 
 class STMessenger (object):
 
@@ -33,38 +34,48 @@ class STMessenger (object):
 
     def send_event_update(self, event_list):
         """Send an event update to the scanning thread.
+
         :param event_list: tuple of event name and state
         :returns: None
         :raises: ValueError if event_list is mal-formed.
         """
+
         if isinstance(event_list, tuple) and len(event_list) == 2:
             self.mqueue.send_to_child(event_list)
         else:
+            logger.error:("Event list:{}".format(event_list))
             raise ValueError("Bad event update attempt.")
 
     def update_queued(self):
         """Check is there is an event update waiting to be processed.
         :returns: True if event waiting
         """
+
         return self.mqueue.queued_for_child()
 
     def get_event_update(self):
         """Get the next event waiting to be processed.
+
         :returns: event_list or None
         """
+
         if self.mqueue.queued_for_child():
             return self.mqueue.get_from_child()
 
     def notify_end_of_scan(self):
         """Notify main thread that scanning thread has terminated.
+
         :returns: None
         """
+
         self.mqueue.signal_parent(1)
 
     def check_end_of_scan(self):
         """ Check to see if the scanning thread as notified us of its termination.
+
         :returns: True if termination signal sent.
         """
+
         if self.mqueue.get_from_parent() == 1:
             return True
         else:
