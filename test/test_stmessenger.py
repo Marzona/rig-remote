7c1315cb86e8ce0af71ec8ecdd122e95a3ce3f89
Simone Marzona <marzona@knoway.info>
2016-04-28 07:27:28 +0100

    started to work on unit tests

---
+++ test/test_stmessenger.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+# import modules
+import pytest
+from modules.stmessenger import STMessenger
+
+fake_event_list=[("test"), ([1,2,3])]
+@pytest.mark.parametrize("fake_event", fake_event_list)
+def test_wrong_event_update(fake_event):
+    fake=fake_event
+    stm = STMessenger()
+    with pytest.raises(ValueError):
+        stm.send_event_update(fake)
+
+def test_good_event_update():
+    fake=(1,2)
+    stm = STMessenger()
+    assert (stm.send_event_update(fake) == None)
+
+class fake_queue_comms():
+    def get_from_child():
+        raise Exception
+
+def wrong_get_event_update(fake_queue_comms):
+    stm = STMessenger
+    stm.mqueue = fake_queue_comms
+    assert (stm.send_event_update(fake) == [])
+
