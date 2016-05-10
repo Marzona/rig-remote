9a46eaee2f6b668a4dd93519dcc2001243277dee
Simone Marzona <simone.marzona@knoway.info>
2016-05-10 20:58:20 +0100

    silly update to release.sh, added more fine grained exception handling in is_hostname_valid, added unit tests around is_hostname_valid

--- release.sh
+++ release.sh
@@ -1,7 +1,14 @@
+#!/bin/bash
+
+echo "This script will perform the unit tests and build the egg. It will ask for confirmation before starting the upload."
+
 python setup.py test
 python setup.py develop  && python setup.py sdist && python setup.py bdist_egg 
-echo "egg generated. ready to upload?"
+echo "egg generated. Press enter if you are ready to upload?"
 
 read a
 
 python setup.py sdist upload
+
+
+echo "Upload should be completed, please review it at https://pypi.python.org/pypi/rig-remote/2.0"
