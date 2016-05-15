8039d6ef70a8d72fd1ef622f4aaa79d7e1abdfef
Simone Marzona <marzona@knoway.info>
2016-05-15 11:33:52 +0100

    moved part of the code inside while true in scanning.py to another function, started to write tests for the new function

--- test/test_appconfig.py
+++ test/test_appconfig.py
@@ -22,3 +22,5 @@ def test_app_config4():
     ac = AppConfig("/tmp/test")
     ac.read_conf()
     assert(len(ac.config) == 16)
+
+
--- test/test_rigctl.py
+++ test/test_rigctl.py
@@ -25,4 +25,13 @@ def test_get_connection_refused(hostname, port):
     with pytest.raises(socket.error):
         rigctl.get_frequency()
 
+def test_set_frequency():
+    rigctl = RigCtl()
+    with pytest.raises(ValueError):
+        rigctl.set_frequency("test")
+
+def test_set_mode():
+    rigctl = RigCtl()
+    with pytest.raises(ValueError):
+        rigctl.set_mode(5)
 
