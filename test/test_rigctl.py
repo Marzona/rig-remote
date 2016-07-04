19d59d200507e6b08b5b0de0ce083bf384714bed
Simone Marzona <simone.marzona@knoway.info>
2016-07-04 09:09:52 +0100

    added unit tests for rigctl module

--- test/test_rigctl.py
+++ test/test_rigctl.py
@@ -4,7 +4,12 @@
 import pytest
 import socket
 from rig_remote.rigctl import RigCtl
-from rig_remote.constants import DEFAULT_CONFIG
+from rig_remote.constants import (
+                                 DEFAULT_CONFIG,
+                                 ALLOWED_VFO_COMMANDS,
+                                 ALLOWED_SPLIT_MODES,
+                                 RESET_CMD_DICT,
+                                 )
 
 testdata = [("test", "80"), ("127.0.0.1","test"), ("test", "test")]
 testdata2 = [("F 100000"), ("M 100000")]
@@ -35,3 +40,87 @@ def test_set_mode():
     with pytest.raises(ValueError):
         rigctl.set_mode(5)
 
+def test_rig_reset():
+    rigctl = RigCtl()
+    with pytest.raises(ValueError):
+        rigctl.rig_reset("testreset")
+
+def test_set_antenna():
+    rigctl = RigCtl()
+    with pytest.raises(ValueError):
+        rigctl.set_antenna("testreset")
+
+def test_xit():
+    rigctl = RigCtl()
+    with pytest.raises(ValueError):
+        rigctl.set_xit(22)
+
+def test_vfo_1():
+    rigctl = RigCtl()
+    with pytest.raises(ValueError):
+        rigctl.set_vfo(22)
+
+def test_vfo_2():
+    rigctl = RigCtl()
+    with pytest.raises(ValueError):
+        rigctl.set_vfo("testvfo")
+
+def test_split_mode_1():
+    rigctl = RigCtl()
+    with pytest.raises(ValueError):
+        rigctl.set_split_mode(22)
+
+def test_split_mode_2():
+    rigctl = RigCtl()
+    with pytest.raises(ValueError):
+        rigctl.set_split_mode("testvfo")
+
+def test_split_freq():
+    rigctl = RigCtl()
+    with pytest.raises(ValueError):
+        rigctl.set_split_freq("testvfo")
+
+def test_rit():
+    rigctl = RigCtl()
+    with pytest.raises(ValueError):
+        rigctl.set_rit("testrit")
+
+def test_parm_1():
+    rigctl = RigCtl()
+    with pytest.raises(ValueError):
+        rigctl.set_parm(22)
+
+def test_parm_2():
+    rigctl = RigCtl()
+    with pytest.raises(ValueError):
+        rigctl.set_parm("testparm")
+
+def test_mode_1():
+    rigctl = RigCtl()
+    with pytest.raises(ValueError):
+        rigctl.set_mode(22)
+
+def test_mode_2():
+    rigctl = RigCtl()
+    with pytest.raises(ValueError):
+        rigctl.set_mode("testmode")
+
+def test_func_1():
+    rigctl = RigCtl()
+    with pytest.raises(ValueError):
+        rigctl.set_func(22)
+
+def test_func_2():
+    rigctl = RigCtl()
+    with pytest.raises(ValueError):
+        rigctl.set_func("testvfo")
+
+def test_antenna():
+    rigctl = RigCtl()
+    with pytest.raises(ValueError):
+        rigctl.set_antenna("testparm")
+
+def test_rig_reset():
+    rigctl = RigCtl()
+    with pytest.raises(ValueError):
+        rigctl.set_antenna("testparam")
