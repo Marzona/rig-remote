769d9041f102d6c7c615951ee88d53f904022cda
Simone Marzona <marzona@knoway.info>
2016-07-05 11:08:29 +0100

    added unit tests for the utility module

---
+++ test/test_utility.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import pytest
+from rig_remote.utility import (
+                                this_file_exists,
+                                is_valid_port,
+                                is_valid_hostname,
+                                )
+
+def test_this_file_exist():
+    with pytest.raises(IOError):
+        this_file_exist("/nonexisting")
+
+def test_is_valid_port_1():
+    with pytest.raises(ValueError):
+        is_valid_port("test")
+
+def test_is_valid_port_2():
+    with pytest.raises(ValueError):
+        is_valid_port(1000)
+
+def test_is_valid_hostname():
+    with pytest.raises(ValueError):
+        is_valid_hostname("")
