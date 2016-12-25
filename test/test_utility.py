54bac36d9c48daf1209bb525536dded6c7690697
marzona <marzona@knoway.info>
2016-12-25 09:22:01 +0000

    - added unit tests - removed bad test

--- test/test_utility.py
+++ test/test_utility.py
@@ -6,8 +6,35 @@ from rig_remote.utility import (
                                 is_valid_hostname,
                                 process_path,
                                 frequency_pp_parse,
+                                store_conf,
                                 build_rig_uri,
                                 )
+from rig_remote.ui import RigRemote
+import Tkinter as tk
+from mock import MagicMock
+from rig_remote.app_config import AppConfig
+
+testdata = [("aggr_scan", "false"),
+            ("auto_bookmark", "false"),
+            ("hostname1", ""),
+            ("passes", ""),
+            ("aggr_scan", "false"),
+            ("log", "false"),
+            ("record", "false"),
+            ("range_max", ""),
+            ("log_filename", "none"),
+            ("interval", ""),
+            ("hostname2", ""),
+            ("range_min", ""),
+            ("delay", ""),
+            ("bookmark_filename", "./test/test-bookmarks.csv"),
+            ("sgn_level", ""),
+            ("save_exit", "false"),
+            ("always_on_top", "false"),
+            ("auto_bookmark", "false"),
+            ("wait", "false"),
+            ("port2", ""),
+            ("port1", "")]
 
 def test_this_file_exist():
     assert(None == this_file_exists("/nonexisting"))
@@ -46,3 +73,14 @@ def test_frequency_pp_parse2():
 def test_build_rig_uri():
     with pytest.raises(NotImplementedError):
         build_rig_uri(3,"test")
+
+@pytest.mark.parametrize("key, value", testdata)
+def test_store_conf(key, value):
+    root = tk.Tk()
+    ac = AppConfig("./test/test-config.file")
+    ac.read_conf()
+    rr = RigRemote(root, ac)
+    rr.ac.write_conf = MagicMock()
+    out = store_conf(rr)
+    assert(out.config[key] == value)
+    rr.root.destroy()
