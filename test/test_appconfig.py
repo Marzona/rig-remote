9a2b3b5458407608f7bfd62270fd4c2e8215eca7
Simone Marzona <Marzona@users.noreply.github.com>
2017-01-03 14:16:27 +0000

    Issues 82 (#87)
    * - fixed code for fixing the bug of https://github.com/Marzona/rig-remote/issues/82
    - added unit tests
    - created a few other issue to refactor/document better this part of the codebase
    
    rootdir: /home/marzona, inifile:
    plugins: cov-2.2.1
    collected 197 items
    
    test/test_appconfig.py ......
    test/test_bookmarks.py ...................
    test/test_disk_io.py .........
    test/test_queuecomms.py ..............
    test/test_rigctl.py ................................................
    test/test_scanning.py .....................
    test/test_stmessenger.py .......
    test/test_ui.py ........................................
    test/test_utility.py .................................
    --------------- coverage: platform linux2, python 2.7.12-final-0 ---------------
    Name                        Stmts   Miss  Cover   Missing
    ---------------------------------------------------------
    rig_remote/__init__.py          0      0   100%
    rig_remote/app_config.py       39     18    54%   86-92, 104-117
    rig_remote/bookmarks.py       137     37    73%   66, 88, 108, 110, 113, 115-116, 134-135, 148-168, 182-187, 199-200, 223-224, 237, 245-247, 272
    rig_remote/constants.py        54      0   100%
    rig_remote/disk_io.py          75     16    79%   80, 102, 130, 136-137, 161, 168-179, 187-191
    rig_remote/exceptions.py       13      0   100%
    rig_remote/queue_comms.py      44      0   100%
    rig_remote/rigctl.py          154     15    90%   84-87, 148, 156, 183, 210, 237, 264, 292, 319, 344, 371, 399
    rig_remote/scanning.py        232    106    54%   165, 206, 230-236, 253, 263-264, 267-268, 286-332, 368, 381-427, 440-463
    rig_remote/stmessenger.py      24      1    96%   59
    rig_remote/ui.py              602    194    68%   150-156, 992, 1008-1011, 1030-1031, 1035-1037, 1041-1042, 1053-1054, 1070-1073, 1079-1089, 1097-1106, 1164-1213, 1267-1268, 1275-1282, 1302-1346, 1360, 1393-1402, 1412, 1418-1419, 1425-1426, 1441-1447, 1458-1459, 1465-1466, 1481-1495, 1503, 1509, 1521-1531, 1534-1554, 1560-1561, 1567-1568, 1592-1598, 1617, 1623, 1634-1639
    rig_remote/utility.py         191     21    89%   71, 110-114, 212, 265-266, 293, 304-305, 313, 329, 349-353, 357, 359, 387, 393
    test/test_appconfig.py         24      0   100%
    test/test_bookmarks.py        130      3    98%   45-47
    test/test_disk_io.py           43      0   100%
    test/test_import.py             9      0   100%
    test/test_queuecomms.py        68      0   100%
    test/test_rigctl.py           234      0   100%
    test/test_scanning.py         185      2    99%   59, 72
    test/test_stmessenger.py       29      0   100%
    test/test_ui.py               265      0   100%
    test/test_utility.py           65      0   100%
    ---------------------------------------------------------
    TOTAL                        2617    413    84%
    
    * Update README.md

--- test/test_appconfig.py
+++ test/test_appconfig.py
@@ -4,6 +4,7 @@
 import pytest
 import os
 from rig_remote.app_config import AppConfig
+from rig_remote.constants import DEFAULT_CONFIG
 
 def test_app_config2():
     ac = AppConfig("test")
@@ -14,4 +15,19 @@ def test_app_config4():
     ac.read_conf()
     assert(len(ac.config) == 16)
 
+def test_app_config5():
+    ac = AppConfig("")
+    assert (isinstance (ac.config, dict) == True)
 
+def test_app_config8():
+    ac = AppConfig(DEFAULT_CONFIG)
+    assert(ac.config == {})
+
+def test_app_config9():
+    ac = AppConfig("./test-config.file")
+    ac.read_conf()
+    assert(ac.config["bookmark_filename"] == "/home/marzona/.rig_remote/rig-remote-bookmarks.csv")
+
+def test_app_config6():
+    ac = AppConfig(DEFAULT_CONFIG)
+    assert(ac.config_file["bookmark_filename"] == "")
